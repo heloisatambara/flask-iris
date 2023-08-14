217fc7a4a34250179916858b51cefdc664e4bfb3
heloisatambara <heloisatambara@usp.br>
2023-08-14 15:49:58 -0300

    creating and connecting db (still not iris tho

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,5 +26,9 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, world!'
+    
+    # import the database createdS
+    from . import db
+    db.init_app(app)
 
     return app    
