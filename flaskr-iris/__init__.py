d698c3a342eab79fb3ea67b2ca020811824adde3
heloisatambara <heloisatambara@usp.br>
2023-09-14 12:03:58 -0300

    making init simpler

--- flaskr-iris/__init__.py
+++ flaskr-iris/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from sqlalchemy.exc import DatabaseError
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     
@@ -11,12 +11,7 @@ def create_app(test_config=None):
         SQLALCHEMY_DATABASE_URI = "iris://_SYSTEM:sys@localhost:1972/SAMPLE"
     )
     
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config is passed in
-        app.config.from_mapping(test_config)
+
         
     
     # flask initializes Alchemy with this app
